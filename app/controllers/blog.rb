a2896dc401fb03afc81eb4d7ab743a35325be7ac
aztack <aztack@163.com>
2014-09-24 00:03:07 +0800

    bigbang

---
+++ app/controllers/blog.rb
@@ -0,0 +1,35 @@
+Azblog::App.controllers :blog do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :index do
+    @blogs = Blog.all
+    render 'blog/index'
+  end
+
+  get :new do
+
+  end
+
+  post :create do
+
+  end
+
+end
---
+++ app/helpers/blog_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Azblog
+  class App
+    module BlogHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers BlogHelper
+  end
+end
